c6c514859c79036a0191b0bb9de4409ca1e6f484
ananthu <ask1234560@gmail.com>
2020-03-27 22:58:52 +0530

    sxiv bg selector

--- dotfiles/scripts/bg-changer.sh
+++ dotfiles/scripts/bg-changer.sh
@@ -2,9 +2,15 @@
 
 # hsetroot -fill Arch-Linux-Wallpaper-04-1920x1080.jpg -tint '#9ccfe6' -write Arch-Linux-Wallpaper-04-1920x1080.jpg
 
+if [ $# -ne 0 ]; then
 
 targetDir="/home/ananthu/Pictures/.wallpaper/modified"
-
 bgfile="$( ls -A $targetDir | shuf -n 1 )"
 gsettings set org.gnome.desktop.background picture-uri "file://$targetDir/$bgfile"
+
+else 
+
+find ~/Pictures/Wallpapers/modified ~/Pictures/.wallpaper/modified -type f | shuf | sxiv - -ftbo 2> /dev/null | shuf -n 1 | xargs -i gsettings set org.gnome.desktop.background picture-uri "file://{}"
+
+fi
 
