efeda61dba5e50a451c9d26394eba7e8809723c2
ananthu <ask1234560@gmail.com>
2020-03-31 19:05:24 +0530

    new script

--- dotfiles/scripts/lowpower_hibernate.sh
+++ dotfiles/scripts/lowpower_hibernate.sh
@@ -6,7 +6,7 @@ acpi -b | awk -F'[,:%]' '{print $2, $3}' | {
 	read -r status capacity
 
 	if [ "$status" = Discharging -a "$capacity" -lt 25 ]; then
-		notify-send -t 10000 'Critical battery threshold' 'Connect a power source.'  --icon=battery-low
+		notify-send -t 10000 'Critical battery threshold' 'Connect a power source.'  --icon=battery-caution-symbolic.symbolic
 
 		if [ "$capacity" -lt 15 ]; then
 			logger "Critical battery threshold"
